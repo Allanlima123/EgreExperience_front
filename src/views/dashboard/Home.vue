f446fd1392da00a21f4d2c64b160a28ba50cfaa3
alan lima <alan.silva1f@gmail.com>
2024-08-08 16:13:41 -0300

    Novas modificações no projeto

--- src/views/dashboard/Home.vue
+++ src/views/dashboard/Home.vue
@@ -25,32 +25,231 @@
 
         <div
           v-if="isVisibleForm"
-          class="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full"
+          class="bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto"
         >
-          <h2 class="text-xl font-bold mb-4">Formulário de Perguntas</h2>
+          <h2 class="text-2xl font-bold mb-6">Formulário de Informações</h2>
           <form @submit.prevent="handleSubmit">
-            <div class="mb-4">
-              <label for="question1" class="block text-sm font-medium mb-2">
-                Pergunta 1
-              </label>
-              <input
-                type="text"
-                id="question1"
-                class="border border-gray-300 rounded p-2 w-full"
-                required
-              />
+            <div class="grid grid-cols-6 md:grid-cols-6 gap-4 mb-4">
+              <div class="col-span-3">
+                <label for="nome" class="block text-sm font-medium mb-2"
+                  >Nome</label
+                >
+                <input
+                  type="text"
+                  id="nome"
+                  placeholder="Nome completo"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+
+              <div class="col-span-3">
+                <label for="foto" class="block text-sm font-medium mb-2"
+                  >Foto (URL)</label
+                >
+                <input
+                  type="text"
+                  id="foto"
+                  placeholder="URL da foto"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+
+              <div class="col-span-3">
+                <label for="empresa" class="block text-sm font-medium mb-2"
+                  >Empresa</label
+                >
+                <input
+                  type="text"
+                  id="empresa"
+                  placeholder="Nome da empresa"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+
+              <div class="col-span-3">
+                <label for="cargo" class="block text-sm font-medium mb-2"
+                  >Cargo</label
+                >
+                <input
+                  type="text"
+                  id="cargo"
+                  placeholder="Cargo"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+
+              <div class="col-span-1 md:col-span-2">
+                <label
+                  for="descricaoTecnica"
+                  class="block text-sm font-medium mb-2"
+                  >Descrição Técnica</label
+                >
+                <textarea
+                  id="descricaoTecnica"
+                  placeholder="Descrição técnica"
+                  class="border border-gray-300 rounded p-2 w-full"
+                  rows="4"
+                ></textarea>
+              </div>
+
+              <!-- Nível -->
+              <!-- <div>
+                <label for="nivel" class="block text-sm font-medium mb-2"
+                  >Nível</label
+                >
+                <select
+                  id="nivel"
+                  class="border border-gray-300 rounded p-2 w-full"
+                >
+                  <option
+                    v-for="nivel in nivelOptions"
+                    :key="nivel"
+                    :value="nivel"
+                  >
+                    {{ nivel }}
+                  </option>
+                </select>
+              </div> -->
+
+              <!-- <div class="col-span-1 md:col-span-2">
+                <label for="idiomas" class="block text-sm font-medium mb-2"
+                  >Idiomas</label
+                >
+                <ul class="grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-3">
+                  <li
+                    v-for="idioma in idiomasOptions"
+                    :key="idioma"
+                    class="flex items-center"
+                  >
+                    <input
+                      type="checkbox"
+                      :id="idioma"
+                      :value="idioma"
+                      v-model="selectedIdiomas"
+                      class="mr-2 h-4 w-4 cursor-pointer"
+                    />
+                    <label :for="idioma" class="text-sm">{{ idioma }}</label>
+                  </li>
+                </ul>
+              </div> -->
+
+              <!-- Empresa -->
+
+              <!-- Remoto -->
+              <!-- <div class="flex items-center mb-4">
+                <input type="checkbox" id="remoto" class="mr-2" />
+                <label for="remoto" class="text-sm font-medium"
+                  >Trabalho Remoto</label
+                >
+              </div> -->
+
+              <!-- Cargo -->
+
+              <!-- Tempo -->
+              <!-- <div>
+                <label for="tempo" class="block text-sm font-medium mb-2"
+                  >Tempo</label
+                >
+                <input
+                  type="text"
+                  id="tempo"
+                  placeholder="Tempo"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- Descrição Profissional -->
+              <!-- <div class="col-span-1 md:col-span-2">
+                <label
+                  for="descricaoProficional"
+                  class="block text-sm font-medium mb-2"
+                  >Descrição Profissional</label
+                >
+                <textarea
+                  id="descricaoProficional"
+                  placeholder="Descrição profissional"
+                  class="border border-gray-300 rounded p-2 w-full"
+                  rows="4"
+                ></textarea>
+              </div> -->
+
+              <!-- Modalidade -->
+              <!-- <div>
+                <label for="modalidade" class="block text-sm font-medium mb-2"
+                  >Modalidade</label
+                >
+                <input
+                  type="text"
+                  id="modalidade"
+                  placeholder="Modalidade"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- Ano de Início -->
+              <!-- <div>
+                <label for="anoInicio" class="block text-sm font-medium mb-2"
+                  >Ano de Início</label
+                >
+                <input
+                  type="date"
+                  id="anoInicio"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- Ano de Conclusão -->
+              <!-- <div>
+                <label for="anoConclusao" class="block text-sm font-medium mb-2"
+                  >Ano de Conclusão</label
+                >
+                <input
+                  type="date"
+                  id="anoConclusao"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- CH -->
+              <!-- <div>
+                <label for="ch" class="block text-sm font-medium mb-2"
+                  >CH</label
+                >
+                <input
+                  type="number"
+                  id="ch"
+                  placeholder="Carga horária"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- Email -->
+              <!-- <div>
+                <label for="email" class="block text-sm font-medium mb-2"
+                  >Email</label
+                >
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
+
+              <!-- Password
+              <div>
+                <label for="password" class="block text-sm font-medium mb-2"
+                  >Senha</label
+                >
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Senha"
+                  class="border border-gray-300 rounded p-2 w-full"
+                />
+              </div> -->
             </div>
-            <div class="mb-4">
-              <label for="question2" class="block text-sm font-medium mb-2">
-                Pergunta 2
-              </label>
-              <input
-                type="text"
-                id="question2"
-                class="border border-gray-300 rounded p-2 w-full"
-                required
-              />
-            </div>
+
             <button
               type="submit"
               class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
@@ -58,9 +257,6 @@
               Enviar
             </button>
           </form>
-          <button @click="closePopup" class="mt-4 text-red-500 hover:underline">
-            Cancelar
-          </button>
         </div>
       </div>
     </Teleport>
@@ -141,6 +337,25 @@ import { ref, onMounted } from "vue";
 
 const isVisible = ref(false);
 const isVisibleForm = ref(false);
+const idiomasOptions = [
+  "INGLES",
+  "ESPANHOL",
+  "PORTUGUES",
+  "FRANCES",
+  "ALEMAO",
+  "ITALIANO",
+  "CHINES",
+  "JAPONES",
+  "RUSSO",
+  "ARABE",
+  "HINDI",
+  "COREANO",
+  "TURCO",
+  "HOLANDES",
+  "POLONES",
+];
+
+const nivelOptions = ["BASICO", "INTERMEDIARIO", "AVANCADO"];
 
 const initializePopup = () => {
   try {
